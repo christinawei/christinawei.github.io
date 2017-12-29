7877fffaeb7edbadc5a417d1206307445d5fc196
Christina Wei <tinawei888@gmail.com>
2017-12-28 20:08:09 -0500

    tester template

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+app.get('/', function(req, res){
+	res.sendfile('html/index.html')
+})
 app.listen(port, function() {
 	console.log("Server started, listening on port "+port);
 });
