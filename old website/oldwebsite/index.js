f0ba0108fccf4127722c6ad3d090d416419b41ba
Christina Wei <tinawei888@gmail.com>
2018-08-06 20:04:48 -0400

    deleted old files

--- old website/oldwebsite/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
-	res.sendfile('html/index.html')
-})
-app.listen(port, function() {
-	console.log("Server started, listening on port "+port);
-});
